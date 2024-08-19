c24cb558d2dd47115258124111145fb893cac9e7
sofia va <sophia.lily.lee@gmail.com>
2024-08-19 22:25:54 +0300

    tests run

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BonusService service = new BonusService();
+        long bonus = service.calculate(1_000, true);
+        System.out.println(bonus);
     }
 }
